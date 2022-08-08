9aac80dff7e1d7ac297e1e372b6fbf8769c564ce
Miquel Mayordomo <miquelmayordomo@gmail.com>
2022-08-08 23:12:20 +0200

    lab done without bonuses

---
+++ fist-vue-lab/src/components/CreatedComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="iteration2-text-interpolation">
+        <h1>This is {{myName}}'s first Vue.js lab at Ironhack Bootcamp</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CreatedComponent',
+    data () {
+        return {
+            myName: "Miquel"
+        }
+    }
+}
+</script>
