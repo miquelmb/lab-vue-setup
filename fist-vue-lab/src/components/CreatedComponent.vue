c59f1c105009b9cd46d11236628ffe69a5467c3a
Miquel Mayordomo <miquelmayordomo@gmail.com>
2022-08-09 16:53:35 +0200

    Lab 1 Done + Bonus3

--- fist-vue-lab/src/components/CreatedComponent.vue
+++ fist-vue-lab/src/components/CreatedComponent.vue
@@ -1,16 +1,30 @@
 <template>
-    <div id="iteration2-text-interpolation">
+    <div>
+        <div id="iteration2-text-interpolation">
         <h1>This is {{myName}}'s first Vue.js lab at Ironhack Bootcamp</h1>
     </div>
+
+    <div id="hello-world-button">
+        <button v-on:click="helloWorld()">Click Me</button>
+    </div>
+    </div>
+    
 </template>
 
 <script>
+
+
 export default {
     name: 'CreatedComponent',
     data () {
         return {
-            myName: "Miquel"
+            myName: "Miquel",
         }
+    },
+    methods: {
+        helloWorld() {
+            console.log('Hello World!')
+        },
     }
 }
 </script>
